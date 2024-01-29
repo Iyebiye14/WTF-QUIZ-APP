51866efb366f2fa3e93f0a1def7eb506694677f6
Iyebiye14 <103181394+Iyebiye14@users.noreply.github.com>
2024-01-29 13:47:58 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who was the first and only prime minister of Nigeria?",
+    answer: "Abubakar Tafawa Balewa",
+    options: [
+      "Abubakar Tafawa Balewa",
+      "Ahmadu Bello",
+      "Nnamdi Azikiwe",
+      "Yakubu Gowon"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are the two major religions in Nigeria?",
+    answer: "Christianity and Islam",
+    options: [
+      "Christianity and Traditional",
+      "Christianity and Judism",
+      "Christianity and Islam",
+      "Islam and Hinduism"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who composed the Nigerian national anthem - 'Arise O Compatriots'?",
+    answer: "Taiwo Akinkumi",
+    options: [
+      "Taiwo Akinkumi",
+      "Ben Odiase",
+      "Lillian Jean Williams",
+      "Flora Shaw"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What are the colours of the Nigerian flag?",
+    answer: "Green and white",
+    options: [
+      "Green and Yellow",
+      "Yellow and Red",
+      "Blue and white",
+      "Green and white"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Nigeria is intersected by which two major Rivers?",
+    answer: "River Niger and River Benue",
+    options: [
+      "Imo River and Benue River",
+      "River Niger and River Benue",
+      "River Nile and River Benue",
+      "River Niger and River Congo"
+    ]
+  },
+];
+
+
